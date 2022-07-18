56213a88f905b75561ab8bab9b57f3fc54b6027f
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-18 02:07:09 -0400

    Updates!

--- resize.js
+++ resize.js
@@ -61,9 +61,6 @@ function initResizable()
     var windowWidth = $(window).width() + "px";
     var sidenavWidth = $(sidenav).outerWidth();
     content.css({marginLeft:parseInt(sidenavWidth)+"px"});
-    if (typeof page_layout!=='undefined' && page_layout==1) {
-      footer.css({marginLeft:parseInt(sidenavWidth)+"px"});
-    }
     writeCookie('width',sidenavWidth-barWidth, null);
   }
 
@@ -71,9 +68,6 @@ function initResizable()
   {
     var windowWidth = $(window).width() + "px";
     content.css({marginLeft:parseInt(navWidth)+barWidth+"px"});
-    if (typeof page_layout!=='undefined' && page_layout==1) {
-      footer.css({marginLeft:parseInt(navWidth)+barWidth+"px"});
-    }
     sidenav.css({width:navWidth + "px"});
   }
 
