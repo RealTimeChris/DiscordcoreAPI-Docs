299e7e96bc3f676791d707ada2a8b5f49dac33b7
realtimechris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-18 15:25:09 +0000

    Updates!

--- d0/db6/a00692.js
+++ d0/db6/a00692.js
@@ -1,10 +1,20 @@
 var a00692 =
 [
-    [ "CoRoutineStatus", "d0/db6/a00692.html#ga422a91ba942835ea1809a91d1fe1e1d5", [
-      [ "Idle", "d0/db6/a00692.html#gga422a91ba942835ea1809a91d1fe1e1d5ae599161956d626eda4cb0a5ffb85271c", null ],
-      [ "Running", "d0/db6/a00692.html#gga422a91ba942835ea1809a91d1fe1e1d5a5bda814c4aedb126839228f1a3d92f09", null ],
-      [ "Complete", "d0/db6/a00692.html#gga422a91ba942835ea1809a91d1fe1e1d5aae94f80b3ce82062a5dd7815daa04f9d", null ],
-      [ "Cancelled", "d0/db6/a00692.html#gga422a91ba942835ea1809a91d1fe1e1d5aa149e85a44aeec9140e92733d9ed694e", null ]
+    [ "VoiceActiveState", "d0/db6/a00692.html#a7db510a888c180dab0d18c694308c066", [
+      [ "Connecting", "d0/db6/a00692.html#a7db510a888c180dab0d18c694308c066ae321c53b354930ba96f0243e652df458", null ],
+      [ "Playing", "d0/db6/a00692.html#a7db510a888c180dab0d18c694308c066ac9dbb2b7c84159b632d71e512eba8428", null ],
+      [ "Stopped", "d0/db6/a00692.html#a7db510a888c180dab0d18c694308c066ac23e2b09ebe6bf4cb5e2a9abe85c0be2", null ],
+      [ "Paused", "d0/db6/a00692.html#a7db510a888c180dab0d18c694308c066ae99180abf47a8b3a856e0bcb2656990a", null ],
+      [ "Exiting", "d0/db6/a00692.html#a7db510a888c180dab0d18c694308c066a0657d962b72e6f1f37dda8dad3684cb8", null ]
     ] ],
-    [ "NewThreadAwaitable", "d0/db6/a00692.html#ga7cf0823c4aaed2bf49257a8f2ccd053f", null ]
+    [ "VoiceConnectionState", "d0/db6/a00692.html#afe15cb95a487dea355bdfba55724535d", [
+      [ "Collecting_Init_Data", "d0/db6/a00692.html#afe15cb95a487dea355bdfba55724535da348170ab8d5fadb2ac2d40bc3dca0a2a", null ],
+      [ "Initializing_WebSocket", "d0/db6/a00692.html#afe15cb95a487dea355bdfba55724535da9ecd4ab0f7c51493146ec71cae31ae03", null ],
+      [ "Collecting_Hello", "d0/db6/a00692.html#afe15cb95a487dea355bdfba55724535da90f04d33ff20d8c558d7fc2d785a715b", null ],
+      [ "Sending_Identify", "d0/db6/a00692.html#afe15cb95a487dea355bdfba55724535da7e98b13b3e8895e0d9b06562f1fbf3bd", null ],
+      [ "Collecting_Ready", "d0/db6/a00692.html#afe15cb95a487dea355bdfba55724535da360572087d671fb700b40bd555050f69", null ],
+      [ "Initializing_DatagramSocket", "d0/db6/a00692.html#afe15cb95a487dea355bdfba55724535daa4c704063d3894a34897f424fbb6e1ba", null ],
+      [ "Sending_Select_Protocol", "d0/db6/a00692.html#afe15cb95a487dea355bdfba55724535dafe47ea48e4a9d0424e3354d4a5e10bfe", null ],
+      [ "Collecting_Session_Description", "d0/db6/a00692.html#afe15cb95a487dea355bdfba55724535dada02982bc0569abf1fba40d83e4332d8", null ]
+    ] ]
 ];
--- search/files_0.js
+++ search/files_0.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['applicationcommandentities_2ecpp_0',['ApplicationCommandEntities.cpp',['../da/d96/a00782.html',1,'']]],
-  ['applicationcommandentities_2ehpp_1',['ApplicationCommandEntities.hpp',['../dc/de4/a00677.html',1,'']]],
-  ['audiodecoder_2ecpp_2',['AudioDecoder.cpp',['../d3/d00/a00785.html',1,'']]],
-  ['audiodecoder_2ehpp_3',['AudioDecoder.hpp',['../d4/d9b/a00680.html',1,'']]],
+  ['applicationcommandentities_2ecpp_0',['ApplicationCommandEntities.cpp',['../d4/d45/a00791.html',1,'']]],
+  ['applicationcommandentities_2ehpp_1',['ApplicationCommandEntities.hpp',['../d8/db9/a00725.html',1,'']]],
+  ['audiodecoder_2ecpp_2',['AudioDecoder.cpp',['../dc/d17/a00872.html',1,'']]],
+  ['audiodecoder_2ehpp_3',['AudioDecoder.hpp',['../dd/dd8/a00719.html',1,'']]],
   ['audioencoder_2ecpp_4',['AudioEncoder.cpp',['../df/d0c/a00788.html',1,'']]],
   ['audioencoder_2ehpp_5',['AudioEncoder.hpp',['../db/d77/a00683.html',1,'']]],
-  ['automoderationentities_2ecpp_6',['AutoModerationEntities.cpp',['../d4/d45/a00791.html',1,'']]],
-  ['automoderationentities_2ehpp_7',['AutoModerationEntities.hpp',['../df/d9e/a00686.html',1,'']]]
+  ['automoderationentities_2ecpp_6',['AutoModerationEntities.cpp',['../d9/daf/a00830.html',1,'']]],
+  ['automoderationentities_2ehpp_7',['AutoModerationEntities.hpp',['../d8/dba/a00773.html',1,'']]]
 ];
