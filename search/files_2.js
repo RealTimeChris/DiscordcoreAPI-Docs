79155bd18d2c18019f87fb4402cfa243ec475acb
realtimechris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-19 05:43:39 +0000

    Updates!

--- search/files_2.js
+++ search/files_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['dataparsingfunctions_2ecpp_0',['DataParsingFunctions.cpp',['../df/d0e/a00800.html',1,'']]],
-  ['discordcoreclient_2ecpp_1',['DiscordCoreClient.cpp',['../d4/d45/a00791.html',1,'']]],
-  ['discordcoreclient_2ehpp_2',['DiscordCoreClient.hpp',['../d6/dc0/a00734.html',1,'']]],
-  ['dllmain_2ecpp_3',['DllMain.cpp',['../d0/d82/a00857.html',1,'']]]
+  ['dataparsingfunctions_2ecpp_0',['DataParsingFunctions.cpp',['../d6/d2f/a00854.html',1,'']]],
+  ['discordcoreclient_2ecpp_1',['DiscordCoreClient.cpp',['../d8/db7/a00812.html',1,'']]],
+  ['discordcoreclient_2ehpp_2',['DiscordCoreClient.hpp',['../d1/d56/a00707.html',1,'']]],
+  ['dllmain_2ecpp_3',['DllMain.cpp',['../d7/d49/a00863.html',1,'']]]
 ];
