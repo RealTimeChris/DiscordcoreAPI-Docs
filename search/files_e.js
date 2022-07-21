5b2f1ea75fadda41cbd254b5272c56f8acd1112e
realtimechris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-21 02:35:16 +0000

    Updates!

--- search/files_e.js
+++ search/files_e.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['webhookentities_2ecpp_0',['WebHookEntities.cpp',['../d0/dad/a00806.html',1,'']]],
-  ['webhookentities_2ehpp_1',['WebHookEntities.hpp',['../d4/dfa/a00746.html',1,'']]],
-  ['websocketentities_2ecpp_2',['WebSocketEntities.cpp',['../dc/d1b/a00827.html',1,'']]],
-  ['websocketentities_2ehpp_3',['WebSocketEntities.hpp',['../dd/d57/a00701.html',1,'']]]
+  ['webhookentities_2ehpp_1',['WebHookEntities.hpp',['../d1/d04/a00713.html',1,'']]],
+  ['websocketentities_2ecpp_2',['WebSocketEntities.cpp',['../dc/d6e/a00833.html',1,'']]],
+  ['websocketentities_2ehpp_3',['WebSocketEntities.hpp',['../df/d3b/a00728.html',1,'']]]
 ];
